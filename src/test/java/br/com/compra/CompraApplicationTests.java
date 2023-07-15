e8f337e0d15c47bcbfad4af61c9c8f4b87d11e34
noefernandes <noepessoa@outlook.com>
2023-07-15 12:23:49 -0300

    Adiciona testes

--- src/test/java/br/com/compra/CompraApplicationTests.java
+++ src/test/java/br/com/compra/CompraApplicationTests.java
@@ -1,13 +1,23 @@
 package br.com.compra;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CompraApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void testHealth() {
+        assertThat(restTemplate.getForObject("http://localhost:8080/health",
+                String.class)).contains("Estou funcionando!");
     }
 
 }
