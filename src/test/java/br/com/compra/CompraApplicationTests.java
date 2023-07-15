a2b4c789f26ddaa20f558fef84de315cf00508d3
noefernandes <noepessoa@outlook.com>
2023-07-15 12:39:38 -0300

    Adiciona testes

--- src/test/java/br/com/compra/CompraApplicationTests.java
+++ src/test/java/br/com/compra/CompraApplicationTests.java
@@ -3,20 +3,24 @@ package br.com.compra;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CompraApplicationTests {
+
+    @Value(value="${local.server.port}")
+    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     void testHealth() {
-        assertThat(restTemplate.getForObject("http://localhost:8080/health",
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/health",
                 String.class)).contains("Estou funcionando!");
     }
 
